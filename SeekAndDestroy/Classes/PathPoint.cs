895d7ac45e03e862e54970e1ed2cf28d50df6afb
roshoIvan <rosho.ivan@gmail.com>
2026-02-20 16:42:21 +0100

    Init

---
+++ SeekAndDestroy/Classes/PathPoint.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeekAndDestroy.Classes {
+    public class PathPoint {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public PathPoint(double x, double y) { X = x; Y = y; }
+    }
+}
---
+++ SeekAndDestroy/Classes/SearchPath.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeekAndDestroy.Classes {
+    public class SearchPath {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<PathPoint> Points { get; set; }
+    }
+}
